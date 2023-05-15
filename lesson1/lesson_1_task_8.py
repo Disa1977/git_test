35e4fa5a24f843ad90d78d8dbf4656a1daf6330d
D1sa1977 <petrunichkin1977@gmail.com>
2023-05-15 09:38:01 +0300

    1

--- lesson1/lesson_1_task_8.py
+++ lesson1/lesson_1_task_8.py
@@ -1,14 +1,2 @@
 def num():
-    print(8)
-    print(8)
-    print(0)
-    print(0)
-    print(5)
-    print(5)
-    print(5)
-    print(3)
-    print(5)
-    print(3)
-    print(5)
-
-num() 
+    print()

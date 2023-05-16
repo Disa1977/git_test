7808b62f0e594a89ee13388d52d253f1b9e21ee5
D1sa1977 <petrunichkin1977@gmail.com>
2023-05-16 16:43:42 +0300

    1

--- lesson1/lesson_1_task_7.py
+++ lesson1/lesson_1_task_7.py
@@ -5,13 +5,13 @@ def funcB():
     print(2)
 
 def funcC():
-    print(3)
+    print(3, end='')
 
 def funcD():
     print(4)
 
 def funcE():
-    print(5)
+    print(5, end='')
 
 def funcF():
     print(6)
@@ -20,13 +20,13 @@ def funcG():
     print(7)
 
 def funcH():
-    print(8)
+    print(8, end='')
 
 def funcI():
     print(9)
 
 def funcJ():
-    print(0)
+    print(0, end='')
 
 funcH()
 funcH()

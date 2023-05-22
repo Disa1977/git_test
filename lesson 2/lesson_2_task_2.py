04eb647ed12ff745e2d0364c6a1a8519fea4cf0a
D1sa1977 <petrunichkin1977@gmail.com>
2023-05-22 16:15:56 +0300

    1

--- lesson 2/lesson_2_task_2.py
+++ lesson 2/lesson_2_task_2.py
@@ -1,9 +1,6 @@
-sgs = int(input("Год: "))
+n = int(input("Год: "))
 
-def is_year_lear():
-    for n in sgs: 
-        if n % 4 == 0:
-            print(n)    
-
-is_year_lear()
- 
+if n % 4 == 0:
+    print(f'{"Год "}{n}{": True"}')    
+else:
+    print (f'{"Год "}{n}{": False"}')

94831c68efbb7c8ee053ac5151fdf3ed0218ced5
D1sa1977 <petrunichkin1977@gmail.com>
2023-05-22 11:36:58 +0300

    1

---
+++ lesson 2/lesson_2_task_2.py
@@ -0,0 +1,9 @@
+is_year_lear = input('Год: ')
+is_year_lear = int(is_year_lear)
+
+for n in is_year_lear():
+    if (n % 2 == 0):
+        print(n)    
+
+
+ 
---
+++ lesson 2/lesson_2_task_3.py
@@ -0,0 +1,7 @@
+number0 = input("Сторона квадрата: ")
+number = int(number0)
+
+def sguare(number):
+    print(number ** 2)
+
+sguare(number)
